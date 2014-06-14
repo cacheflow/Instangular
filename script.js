b80f90562f38aaaa88963eef377e4325c8a58b28
Lex Alexander <l.alexander10@gmail.com>
2014-06-13 18:00:05 -0700

    almost done just have to align the grid

---
+++ script.js
@@ -0,0 +1,39 @@
+var app = angular.module("switchableGrid", ["ngResource"]);
+
+app.factory("instagram", function($resource){
+	
+return {
+	fetchPopular: function(callback){
+
+var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK',{
+				client_id: '642176ece1e7445e99244cec26f4de1f'
+			},{
+
+fetch: {method: "JSONP"}
+});
+
+
+api.fetch(function(response){
+
+callback(response.data);
+
+});
+		}
+	}
+
+});
+
+
+function SwitchableGridController($scope, instagram){
+
+$scope.layout = "grid"; 
+$scope.pics = [];
+
+
+instagram.fetchPopular(function(data) {
+
+$scope.pics = data; 
+
+});
+
+}
